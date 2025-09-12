552180c7b0a049571c1dc0f229091f0fb890cb0b
Talimul212 <islamtalimul0@gmail.com>
2025-09-13 01:08:41 +0600

    setup commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { div } from "framer-motion/client";
+import Image from "next/image";
+import Main from "../app/Home/Home";
+
+export default function Home() {
+  return (
+    <div>
+      <Main />
+    </div>
+  );
+}
